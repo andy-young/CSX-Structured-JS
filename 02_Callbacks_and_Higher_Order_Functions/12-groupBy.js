91198a70d58f8ceeb89e945171676784840a7313
Andy Young <ayscript@gmail.com>
2018-12-17 03:13:07 -0700

    Complete Callbacks and Higher Order Functions

---
+++ 02_Callbacks_and_Higher_Order_Functions/12-groupBy.js
@@ -0,0 +1,12 @@
+// Create a function groupBy that accepts an array and a callback, and returns an object. groupBy will iterate through the array and perform the callback on each element. Each return value from the callback will be saved as a key on the object. The value associated with each key will be an array consisting of all the elements that resulted in that return value when passed into the callback.
+
+function groupBy(arr, cb) {
+	return arr.reduce((acc, curr) => {
+		acc.hasOwnProperty(cb(curr)) ? acc[cb(curr)].push(curr) : acc[cb(curr)] = [curr];
+			return acc
+	}, {});
+}
+
+var decimals = [1.3, 2.1, 2.4];
+var floored = function(num) { return Math.floor(num); };
+console.log(groupBy(decimals, floored)); // should log: { 1: [1.3], 2: [2.1, 2.4] }
