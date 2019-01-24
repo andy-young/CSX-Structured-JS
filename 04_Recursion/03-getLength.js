d2baf77d72a737be01acfa8d811ae6001a6791d7
Andy Young <ayscript@gmail.com>
2019-01-24 01:34:51 -0700

    Update 03-getLength.js

--- 04_Recursion/03-getLength.js
+++ 04_Recursion/03-getLength.js
@@ -1,14 +1,15 @@
-// Get the length of an array using recursion without accessing its length property.
-
-function getLength(array, count = 0) {
-  if (array[count] || array.indexOf(undefined) === count) {
-    count++;
-    return getLength(array, count);
-  } else return count;
+function getLength(arr, i = 0) {
+    if (arr.indexOf(undefined) > -1 || arr[0] || arr[0] !== arr[0]) {
+			arr.pop();
+      return getLength(arr, ++i);
+    } else return i;
 }
 
 console.log(getLength([1])); // -> 1
 console.log(getLength([1, 2])); // -> 2
 console.log(getLength([1, 2, 3, 4, 5])); // -> 5
 console.log(getLength([])); // -> 0
+console.log(getLength([NaN])) // -> 1
+console.log(getLength([NaN, NaN, NaN])) // -> 3
 console.log(getLength([undefined])); // -> 1
+console.log(getLength([undefined, undefined, undefined])); // -> 3
