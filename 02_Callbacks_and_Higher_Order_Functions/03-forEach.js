3fb910ec1333c4440bd834651b3fb41c3316383e
Andy Young <ayscript@gmail.com>
2019-03-13 18:43:05 -0600

    Update 03-forEach.js

--- 02_Callbacks_and_Higher_Order_Functions/03-forEach.js
+++ 02_Callbacks_and_Higher_Order_Functions/03-forEach.js
@@ -15,18 +15,32 @@ Part 2 -----------------------------------------------------------------------
 Now let's rebuild map from challenge 1. This time instead of using a for loop, you're going to use the forEach we just created.
 
 */
-function forEach(arr, callback) {
-  for (let i = 0; i < arr.length; i++) {
-    callback(arr[i]);
+function forEach(collection, callback) {
+  if (Array.isArray(collection)) {
+    for (let i = 0; i < collection.length; i++) {
+      callback(collection[i], i);
+    }
+  } else {
+    for (var key in collection) {
+      callback(collection[key], key);
+    }
   }
 }
 
-function map(arr, callback) {
-  forEach(arr, () => arr.push(callback(arr.shift())));
-  return arr;
+function map(collection, callback) {
+  let acc = [];
+
+  if (Array.isArray(collection)) {
+		forEach(collection, (el, i) => acc.push(callback(el, i)));
+  } else {
+  	  acc = {};
+	    forEach(collection, (el, key) => acc[key] = callback(el, key));
+	}
+  return acc;
 }
 
 console.log(typeof forEach); // should log: 'function'
-forEach(['a','b','c'], i => console.log(i)); // should log: 'a', 'b', 'c'
+forEach(['a', 'b', 'c'], i => console.log(i)); // should log: 'a', 'b', 'c'
 console.log(typeof map); // should log: 'function'
-console.log(map([3,4,5], n => n - 2)); // should log: [1, 2, 3]
+console.log(map([3, 4, 5], n => n - 2)); // should log: [1, 2, 3]
+console.log(map({a: 1, b: 2, c: 3}, num => num ** 2)); // should log: { a:1, b: 4, c: 9 }
